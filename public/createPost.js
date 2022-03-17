21262bc5a614e5cdca61a8dd3ea1499685f1f88b
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-03-17 20:43:53 +0000

    added container to store uploader files

--- public/createPost.js
+++ public/createPost.js
@@ -1,13 +1,21 @@
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+  let length = 0;
+  const container = []
     const dropZoneElement = inputElement.closest(".drop-zone");
   
     dropZoneElement.addEventListener("click", (e) => {
-      inputElement.click();
+      inputElement.click();     
     });
   
     inputElement.addEventListener("change", (e) => {
       if (inputElement.files.length) {
-        updateThumbnail(dropZoneElement, inputElement.files[0]);
+        if(length < 5){
+         updateThumbnail(dropZoneElement, inputElement.files[0]);
+         length = length + 1;
+         container.push(inputElement.files[0]);
+        }else{
+          console.log("Exceeded the limit");
+        }
       }
     });
   
@@ -21,19 +29,43 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
         dropZoneElement.classList.remove("drop-zone--over");
       });
     });
-  
     dropZoneElement.addEventListener("drop", (e) => {
       e.preventDefault();
   
-      if (e.dataTransfer.files.length) {
-        inputElement.files = e.dataTransfer.files;
-        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      if (e.dataTransfer.files.length) {        
+        if( length < 5){
+          inputElement.files = e.dataTransfer.files;
+          container.push(inputElement.files[0])
+          updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+          length = length + 1;
+        }else{
+          console.log("Please you can't add more than 4 files!")
+        }
       }
   
       dropZoneElement.classList.remove("drop-zone--over");
     });
+    length = length + 1;
   });
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /**
    * Updates the thumbnail on a drop zone element.
    *
@@ -44,12 +76,12 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
   
     // First time - remove the prompt
-    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    if (dropZoneElement.querySelector(".drop-zone__prompt") && length < 5) {
       dropZoneElement.querySelector(".drop-zone__prompt").remove();
     }
   
     // First time - there is no thumbnail element, so lets create it
-    if (!thumbnailElement) {
+    if (!thumbnailElement && length < 5) {
       thumbnailElement = document.createElement("div");
       thumbnailElement.classList.add("drop-zone__thumb");
       dropZoneElement.appendChild(thumbnailElement);
@@ -58,9 +90,8 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     thumbnailElement.dataset.label = file.name;
   
     // Show thumbnail for image files
-    if (file.type.startsWith("image/")) {
+    if (file.type.startsWith("image/") && length < 5) {
       const reader = new FileReader();
-  
       reader.readAsDataURL(file);
       reader.onload = () => {
         thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
